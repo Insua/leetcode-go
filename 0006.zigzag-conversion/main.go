4fa8b639ea6ab9a1693cb7f3f037d5c1ad2edd3b
insua <insua@sohu.com>
2022-02-11 20:05:37 +0800

    feat(zigzag):

--- 0006.zigzag-conversion/main.go
+++ 0006.zigzag-conversion/main.go
@@ -1,20 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func convert(s string, numRows int) string {
-	var slice [][]string
-	for _, v := range s {
-		fmt.Println(string(v))
+	if numRows == 1 {
+		return s
 	}
-	fmt.Println(slice)
-	return ""
+	period := 2*numRows - 2
+	ss := make([]string, numRows)
+	for k, v := range s {
+		periodNum := (k + 1) % period
+		if periodNum == 0 {
+			periodNum = period
+		}
+		if periodNum <= numRows {
+			ss[periodNum-1] += string(v)
+		} else {
+			ss[period+2-periodNum-1] += string(v)
+		}
+	}
+	zs := ""
+	for _, v := range ss {
+		zs += v
+	}
+	return zs
 }
 
 func main() {
 	s := "PAYPALISHIRING"
 	numRows := 3
-	convert(s, numRows)
+	fmt.Println(convert(s, numRows))
 }
